83f1301c32fd7f6beacd2a60c96dc0f0d5dc03a6
me_chelle <rcjayme@up.edu.ph>
2024-05-30 03:04:20 +0800

    small changes

--- catch_logbook.php
+++ catch_logbook.php
@@ -32,11 +32,44 @@ if(isset($_POST["submit"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catch Logbook</title>
     <link rel="stylesheet" href="stylefish.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
     <header>
         <h1>Fisherman Website</h1>
     </header>
+    <nav class="navbar navbar-expand-lg bg-body-primary">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile_management.php">Profile Management</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="fishing_zone">Fishing Zones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="catch_logbook.php">Catch Logbook</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="fish_market_prices.php">Fish Market Prices</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="equipment">Equipment</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="equipment_management">Equipment Management</a>
+                </li>
+
+              </ul>            
+            </div>         
+          </div>
+      </nav>
     <main>
         <h2>Catch Logbook</h2>
         
@@ -60,6 +93,9 @@ if(isset($_POST["submit"])){
     <footer>
         <p>&copy; 2024 Fisherman Website</p>
     </footer>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script> -->
 
 </body>
 </html>
