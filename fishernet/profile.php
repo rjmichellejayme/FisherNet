a0ab549f1dfda8885e8079be7be91657740ff7e4
me_chelle <rcjayme@up.edu.ph>
2024-06-05 10:11:31 +0800

    Update profile.php

--- fishernet/profile.php
+++ fishernet/profile.php
@@ -10,20 +10,15 @@ if (isset($_POST["submit"])) {
     $name = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $confirmpassword = $_POST['confirmpassword'];
     $userType = $_POST['userType'];
     $duplicate = mysqli_query($conn, "SELECT * FROM users WHERE Username = '$name' OR email = '$email'");
     
     if (mysqli_num_rows($duplicate) > 0) {
         echo "<script> alert('Username or Email is Already Taken'); </script>";
     } else {
-        if ($password == $confirmpassword) {
-            $query = "INSERT INTO users (username, email, password, confirmpassword, usertype) VALUES ('$name', '$email', '$password', '$userType')";
+            $query = "INSERT INTO users (username, email, password, usertype) VALUES ('$name', '$email', '$password', '$userType')";
             mysqli_query($conn, $query);
             echo "<script> alert('Registration Successful'); </script>";
-        } else {
-            echo "<script> alert('Password Does not Match'); </script>";
-        }
     }
 }
 ?>
@@ -49,8 +44,6 @@ if (isset($_POST["submit"])) {
             <input type="email" id="email" name="email" required><br>
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required><br>
-            <label for="confirmpassword">Confirm Password:</label>
-            <input type="password" id="confirmpassword" name="confirmpassword" required><br>
             
             <label for="userType">User Type:</label>
             <select id="userType" name="userType" required>
